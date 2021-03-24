99b3b7aeac28199ee3adafe5a3bb4df1a4bff482
Bassem Aly <basim.alyy@gmail.com>
2021-03-24 18:18:08 +0300

    Create devices.py

---
+++ devices.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Bassem Aly"
+__email__ = "babdelmageed@juniper.net"
+__company__ = "Juniper Networks"
+__version__ = 0.1
+# -----
+
+username = "admin"
+password = "admin123"
+nodes_inventory = {
+        "Gateway1": "192.168.1.8",
+        "Gateway2": "192.168.1.5",
+        "Leaf1": "192.168.1.3",
+        "Leaf2": "192.168.1.4",
+        "Border_Leaf1": "192.168.1.9",
+}
